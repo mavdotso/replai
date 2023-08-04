d3e09f4844f5fc655415652d7bd810381fb1f8a6
0xmav <vladislav.f.denisov@gmail.com>
2023-08-04 17:55:50 +0200

    CORS

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/api/openai*',
+                source: '/api/:path*',
                 headers: [
                     { key: 'Access-Control-Allow-Credentials', value: 'true' },
                     { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
