8cc07f6d5eea03e9daef0a1bcdaf9c42db2c4672
0xmav <vladislav.f.denisov@gmail.com>
2023-08-05 20:57:38 +0200

    Installing Resend

--- src/app/api/(email)/email/verify/route.ts
+++ src/app/api/(email)/email/verify/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.SERVICE_EMAIL;
+const fromEmail = process.env.RESEND_SERVICE_EMAIL;
 
 export async function POST() {
     try {
