1b8239f9d27cabce7e8bd487dca748549ea9d74e
0xmav <vladislav.f.denisov@gmail.com>
2023-08-05 21:07:33 +0200

    fixing rawBody for Stripe

--- src/app/api/(email)/email/verify/route.ts
+++ src/app/api/(email)/email/verify/route.ts
@@ -1,14 +1,11 @@
 import { Verify } from '@/components/emails/VerifyTemplate';
 import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.RESEND_SERVICE_EMAIL;
+import { resend, serviceEmail } from '@/lib/resend';
 
 export async function POST() {
     try {
         const data = await resend.emails.send({
-            from: `@replai <${fromEmail}>`,
+            from: `@replai <${serviceEmail}>`,
             to: ['delivered@resend.dev'],
             subject: 'Verify your email address',
             react: Verify({ firstName: 'John', verificationLink: 'verificationLink' }),
