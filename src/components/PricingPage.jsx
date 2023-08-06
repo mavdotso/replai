8203f32492b184158bfc8be8917d58a9ef8af412
0xmav <vladislav.f.denisov@gmail.com>
2023-08-06 12:13:11 +0200

    Checking if the Stripe subscription active

--- src/components/PricingPage.jsx
+++ src/components/PricingPage.jsx
@@ -8,26 +8,31 @@ async function PricingPage() {
         prices.map(async (price) => {
             const product = await stripe.products.retrieve(price.product);
             // Check if the plan is active and return it
-            if (price.active) {
-                // TODO: Check monthly / yearly prices and return to the diff cards
-
-                return {
-                    id: price.id,
-                    name: product.name,
-                    description: product.description,
-                    price: price.unit_amount / 100,
-                    interval: price.recurring.interval,
-                    currency: price.currency,
-                    metadata: product.metadata,
-                    lookupKey: price.lookup_key,
-                };
-            }
+            // TODO: Check monthly / yearly prices and return to the diff cards
+            return {
+                id: price.id,
+                name: product.name,
+                description: product.description,
+                price: price.unit_amount / 100,
+                interval: price.recurring.interval,
+                currency: price.currency,
+                metadata: product.metadata,
+                lookupKey: price.lookup_key,
+                isActive: product.active,
+            };
         })
     );
 
+    // Check if the plan is active
+    const activePlans = plans.filter((plan) => {
+        if (plan.isActive) {
+            return true;
+        }
+    });
+
     return (
         <div className="grid grid-cols-2 gap-4">
-            {plans
+            {activePlans
                 .slice(0)
                 .reverse()
                 .map((plan) => (
